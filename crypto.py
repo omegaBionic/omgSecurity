62fb8f899e4901c454da38af35502ebf2a98f2dc
omegaBionic <44264108+omegaBionic@users.noreply.github.com>
2018-10-19 09:28:21 +0200

    [Script] Added input function.

--- crypto.py
+++ crypto.py
@@ -5,14 +5,23 @@ import sys
 sys.path.insert(0, 'lib/')
 from cesar import cesar
 
-cryptKey = 10
-sentense = "I'm a computer !"
+cryptKey = int(input("Enter cryptKey in integer: "))
+sentense = input("Enter sentense: ")
 
+
+# Display resume
+print("\nRESUME:")
+print("Your cryptKey is: " + str(cryptKey))
+print("Your sentense is: " + sentense)
+
+# Warnup
 
 
 # Display Cesar encryption
+print("\nCesar encryption:")
 crypticSentense = cesar.encrypt(sentense, cryptKey)
 print("crypticSentense: '" + crypticSentense + "'")
 
 # Display Cesar decryption
+print("\nCesar decryption:")
 print("decryptSentense: '" + cesar.decrypt(crypticSentense, cryptKey) + "'")
